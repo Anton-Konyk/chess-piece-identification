1738105917f66950aea8b1c19ebfb36238c0e175
Anton Konyk <antonkonyk.com>
2025-03-22 20:43:22 +0100

    add static folders

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+STATIC_FOLDER = "static"
+UPLOAD_FOLDER = "static/uploads/"
+
 
 
 if __name__ == '__main__':
