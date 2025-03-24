5db16ea02353e8d5b4de299c6a0ec7cec03448ee
Anton Konyk <antonkonyk.com>
2025-03-24 16:30:39 +0100

    fix syntax

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ MODEL_FOLDER = "static/models/"
 MODEL_FILE = os.path.join(MODEL_FOLDER, "model.keras")
 
 
-url =("https://drive.google.com/uc?export=download&id="
-      "1ksHdcgldcuIBomh_dr-8AAysmyaGtX-F")
+url = ("https://drive.google.com/uc?export=download&id="
+       "1ksHdcgldcuIBomh_dr-8AAysmyaGtX-F")
 
 if not os.path.exists(MODEL_FILE):
     gdown.download(url, MODEL_FILE, quiet=False)
